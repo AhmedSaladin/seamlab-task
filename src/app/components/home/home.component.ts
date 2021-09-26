64296fd8f5b17976ea127852b72e9c8f50bc0ea7
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-26 02:34:58 +0200

    Adding  courses

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,66 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  coureses = [
+    {
+      courseName: 'User Research for User Experience Design',
+      courseColleague: 'The Museum of Modern Art',
+      colleagueLogo: '../../../assets/courses/u.png',
+      courseImg: '../../../assets/courses/URfUED0.png',
+    },
+    {
+      courseName: 'Buddhism and Modern Psychology',
+      courseColleague: 'The Museum of Modern Art',
+      colleagueLogo: '../../../assets/courses/w.png',
+      courseImg: '../../../assets/courses/BaMP.png',
+    },
+    {
+      courseName: 'Introduction to Philosophy',
+      courseColleague: 'Duke University',
+      colleagueLogo: '../../../assets/courses/c.png',
+      courseImg: '../../../assets/courses/ItP.png',
+    },
+    {
+      courseName: 'Advance on Seeing Through Photographs',
+      courseColleague: 'Duke University',
+      colleagueLogo: '../../../assets/courses/u.png',
+      courseImg: '../../../assets/courses/AoSTP.png',
+    },
+    {
+      courseName: 'Think Again I How to Understand Arguments',
+      courseColleague: 'The Museum of Modern Art',
+      colleagueLogo: '../../../assets/courses/c.png',
+      courseImg: '../../../assets/courses/TAIHtUA.png',
+    },
+    {
+      courseName: 'User Research for User Experience Design',
+      courseColleague: 'The Museum of Modern Art',
+      colleagueLogo: '../../../assets/courses/w.png',
+      courseImg: '../../../assets/courses/URfUED1.png',
+    },
+    {
+      courseName: 'User Research for User Experience Design',
+      courseColleague: 'The Museum of Modern Art',
+      colleagueLogo: '../../../assets/courses/u.png',
+      courseImg: '../../../assets/courses/URfUED2.png',
+    },
+    {
+      courseName: 'Introduction to Logic and Critical Thinking',
+      courseColleague: 'Duke University',
+      colleagueLogo: '../../../assets/courses/u.png',
+      courseImg: '../../../assets/courses/URfUED3.png',
+    },
+    {
+      courseName: 'Modern and Contemporary Art and Design',
+      courseColleague: 'The Museum of Modern Art',
+      colleagueLogo: '../../../assets/courses/c.png',
+      courseImg: '../../../assets/courses/MaCAD.png',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
