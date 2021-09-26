b09367cb84688c997d3c5068a657c0ce17c7593b
Ahmed Saladin <as.yuri74@gmail.com>
2021-09-26 02:44:51 +0200

    Update home component html design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  coureses = [
+  courses = [
     {
       courseName: 'User Research for User Experience Design',
       courseColleague: 'The Museum of Modern Art',
